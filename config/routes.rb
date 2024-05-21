6c0f20462fbc07089529e6f9d84032b536eea71f
Amelie Caron <caron.amelie94@gmail.com>
2024-05-21 15:55:09 +0200

    exo2

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "tasks", to: "tasks#index"
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy
+  patch "tasks/:id", to: "tasks#update"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
